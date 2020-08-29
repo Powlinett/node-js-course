a7a4dc0fb5b8db023c18a0190876bb231e755ebb
Pauline <pauline.eustachy@gmail.com>
2020-08-29 16:53:05 +0200

    connect app to mongoDB cluster

--- app.js
+++ app.js
@@ -4,19 +4,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const pagesController = require('./controllers/pages');
+const mongoConnect = require('./helpers/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use('/', pagesController.pageNotFound);
+
+mongoConnect((client) => {
+	console.log(client);
+	app.listen(3000);
+});
--- helpers/database.js
+++ helpers/database.js
@@ -1,8 +1,22 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-js', 'root', '3347:olga', {
-	dialect: 'mysql',
-	host: 'localhost'
-});
+const dotenv = require('dotenv');
 
-module.exports = sequelize;
+dotenv.config();
+
+const mongoConnect = (callback) => {
+	MongoClient
+		.connect(
+			`mongodb+srv://Powlinett:${process.env.MONGODB_PASSWORD}@cluster0.pjc04.mongodb.net/Project0?retryWrites=true&w=majority`
+			)
+			.then((client) => {
+				console.log('connected');
+				callback(client);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+};
+
+module.exports = mongoConnect;
